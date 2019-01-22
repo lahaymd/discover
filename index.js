f93031e05277a5ea871047dc490ad8c150659851
lahaymd <lahaymd@gmail.com>
2019-01-21 21:38:22 -0600

    added function

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ console.log('second log')
 //added comment
 //added comment from topic branch
 const a = {name: 'Mike'}
+function Form(name,age) {
+    this.name = name;s
+    this.age = age;
+    this.fullName = function() {
+        return `${name} ${age}`
+    }
+}
